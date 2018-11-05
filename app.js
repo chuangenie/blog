03f83fd60a27538f1573968e8f40ffaa7398c111
NG <15271906963@163.com>
2018-11-05 21:45:49 +0800

    完成注册登录

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.use('/node_modules', express.static('./node_modules'))
+app.use(bodyParser.urlencoded({ extended: false} ))
 
-app.get('/', (req, res) => {
-    res.render('./index.ejs')
-})
+const indexR = require('./router/index.js')
+app.use(indexR)
 
-
+const userR = require('./router/user.js')
+app.use(userR)
 
 app.listen(80, () => {
     console.log('http://127.0.0.1')
